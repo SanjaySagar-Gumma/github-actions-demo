81ab06e486bdea3ae87049c47dd0de551853a906
sigmarpanda <112972753+sigmarpanda@users.noreply.github.com>
2022-09-09 11:45:42 +0530

    Update azsttrigger.ps1

--- azsttrigger.ps1
+++ azsttrigger.ps1
@@ -5,9 +5,22 @@ $login="azureuser"
 $password="AlfaKumar@1234"
 $storage="gitstorage8"
 $container="gitcontainer"
-
+$server="gitserverdemo"
+$database="gitdbdemo"
+$bacpac="backup.bacpac"
 
 az storage account create --name $storage --resource-group $resourceGroup --location "$location" --sku Standard_LRS
+
 echo "Creating $container on $storage..."
-key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup -o json --query [0].value | tr -d '"')
+$key=$(az storage account keys list --account-name $storage --resource-group $resourceGroup -o json --query [0].value | tr -d '"')
 az storage container create --name $container --account-key $key --account-name $storage
+
+echo "Creating $server in $location..."
+az sql server create --name $server --resource-group $resourceGroup --location "$location" --admin-user $login --admin-password $password
+az sql server firewall-rule create --resource-group $resourceGroup --server $server --name AllowAzureServices --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
+echo "Creating $database..."
+az sql db create --name $database --resource-group $resourceGroup --server $server --edition GeneralPurpose --sample-name AdventureWorksLT
+
+echo "Backing up $database..."
+az sql db export --admin-password $password --admin-user $login --storage-key $key --storage-key-type StorageAccessKey --storage-uri "https://$storage.blob.core.windows.net/$container/$bacpac" --name $database --resource-group $resourceGroup --server $server
